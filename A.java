f31d6d9de8e3fe7fb6df0c521d5ceef208c3e124
sezini <vinicius.sezini@gt4w.com.br>
2020-06-07 19:29:51 -0300

    adicionando classe A e B e seus metodos

---
+++ A.java
@@ -0,0 +1,28 @@
+public class A {
+    private int A1;
+    private float A2;
+
+    public int getA1() {
+        return A1;
+    }
+
+    public int getA2() {
+        return A2;
+    }
+
+    public void setA1(int a1) {
+        this.A1 = a1;
+    }
+
+    public void setA2(float a2) {
+        this.A2 = a2;
+    }
+
+    public static void MA1()  {
+        System.out.println("MA1");
+    }
+
+    public static void MA2()  {
+        System.out.println("MA2");
+    }
+}
---
+++ B.java
@@ -0,0 +1,28 @@
+public class B {
+    private int B1;
+    private float B2;
+
+    public int getB1() {
+        return B1;
+    }
+
+    public int getB2() {
+        return B2;
+    }
+
+    public void setB1(int b1) {
+        this.B1 = b1;
+    }
+
+    public void setB2(float b2) {
+        this.B2 = b2;
+    }
+
+    public static void MB1()  {
+        System.out.println("MB1");
+    }
+
+    public static void MB2()  {
+        System.out.println("MB2");
+    }
+}
