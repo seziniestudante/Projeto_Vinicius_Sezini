810328b6a57dfa47cf3dd9d78093094d52abbf5b
sezini <vinicius.sezini@gt4w.com.br>
2020-06-07 19:39:06 -0300

    Criação da Classe C

---
+++ C.java
@@ -0,0 +1,28 @@
+public class C {
+    private String C1;
+    private int C2;
+
+    public int getC1() {
+        return C1;
+    }
+
+    public int getC2() {
+        return C2;
+    }
+
+    public void setC1(String c1) {
+        this.C1 = c1;
+    }
+
+    public void setC2(int c2) {
+        this.C2 = c2;
+    }
+
+    public static void MC1()  {
+        System.out.println("MC1");
+    }
+
+    public static void MC2()  {
+        System.out.println("MC2");
+    }
+}
