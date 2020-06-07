13113c6d59792a166e274cfa2b85f09e70fb35bd
sezini <vinicius.sezini@gt4w.com.br>
2020-06-07 19:51:34 -0300

    adicionando classe D e seus metodos

---
+++ D.java
@@ -0,0 +1,28 @@
+public class D {
+    private int D1;
+    private float D2;
+
+    public int getD1() {
+        return D1;
+    }
+
+    public int getD2() {
+        return D2;
+    }
+
+    public void setD1(int d1) {
+        this.D1 = d1;
+    }
+
+    public void setD2(float d2) {
+        this.D2 = d2;
+    }
+
+    public static void MD1()  {
+        System.out.println("MD1");
+    }
+
+    public static void MD2()  {
+        System.out.println("MD2");
+    }
+}
